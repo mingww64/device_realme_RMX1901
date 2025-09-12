941ec0f926a9076d538d099b35c106e9a0b8b1cd
Shravan Sayz <shravankumarsayz@gmail.com>
2025-09-12 10:50:02 +0000

    RMX1901: Axi
    Signed-off-by: Shravan Sayz <shravankumarsayz@gmail.com>

--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -4,11 +4,32 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Evolution stuff
+# Inherit some common Axion stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_USES_MINI_GAPPS := true
 TARGET_HAS_UDFPS := true
 TARGET_DISABLE_EPPE := true
+
+# AxionOS-Specific Flags
+AXION_MAINTAINER := Shravan_SK
+AXION_PROCESSOR := Qualcomm_Snapdragon_710
+
+# Define rear camera specs
+AXION_CAMERA_REAR_INFO := 48,5
+AXION_CAMERA_FRONT_INFO := 16
+
+# CPU
+AXION_CPU_SMALL_CORES := 0,1,2,3,4,5
+AXION_CPU_BIG_CORES := 6,7
+AXION_CPU_BG := 0-2
+AXION_CPU_FG := 0-7
+AXION_CPU_LIMIT_BG := 0-1
+AXION_CPU_UNLIMIT_UI := 0-7
+AXION_CPU_LIMIT_UI := 0-5
+AXION_CPU_DISPLAY := 6-7
+AXION_CPU_AUDIO := 0-3
+
+# Blur
+TARGET_ENABLE_BLUR := true
 
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
