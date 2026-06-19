426b7df6e5c6e9557d122bbec75ad3d495448a66
Abdallah Ibrahim <iabdollah40@gmail.com>
2026-06-19 10:28:58 +0000

    RMX1971: Rewrite dolby_shim as self-contained stub (fixes delegation crash)

--- dolby_shim/dolby_shim.cpp
+++ dolby_shim/dolby_shim.cpp
@@ -1,89 +1,117 @@
-#include <dlfcn.h>
-#include <string.h>
 #include <log/log.h>
 #include <hardware/audio_effect.h>
-
-#define LIBSWDAP_PATH "libswdap.so"
-#define LIBSWDAP_SYM  "AELI"
+#include <string.h>
+#include <stdlib.h>
 
 static const effect_uuid_t kUuidDap = {
     0x9d4921da, 0x8225, 0x4f29, 0xaefa,
     {0x39, 0x53, 0x7a, 0x04, 0xbc, 0xaa}
 };
 
-static const effect_uuid_t kUuidType = {
+static const effect_uuid_t kUuidPatchedType = {
     0xec7178ec, 0xe5e1, 0x4432, 0xa3f4,
     {0x46, 0x57, 0xe6, 0x79, 0x52, 0x10}
 };
 
-static void *gLibSwDap = NULL;
-static audio_effect_library_t *gSwDapLib = NULL;
+static int effect_process(effect_handle_t self,
+        audio_buffer_t *inBuffer, audio_buffer_t *outBuffer) {
+    (void)self; (void)inBuffer; (void)outBuffer;
+    return 0;
+}
+
+static int effect_command(effect_handle_t self,
+        uint32_t cmdCode, uint32_t cmdSize,
+        void *pCmdData, uint32_t *replySize, void *pReplyData) {
+    (void)self; (void)cmdSize; (void)pCmdData;
+    switch (cmdCode) {
+    case EFFECT_CMD_INIT:
+    case EFFECT_CMD_SET_CONFIG:
+    case EFFECT_CMD_RESET:
+    case EFFECT_CMD_ENABLE:
+    case EFFECT_CMD_DISABLE:
+    case EFFECT_CMD_SET_PARAM:
+        if (replySize) *replySize = sizeof(int);
+        return 0;
+    case EFFECT_CMD_GET_PARAM:
+        if (replySize) *replySize = sizeof(int);
+        return -ENOSYS;
+    case EFFECT_CMD_SET_DEVICE:
+    case EFFECT_CMD_SET_VOLUME:
+        return 0;
+    default:
+        return 0;
+    }
+}
+
+struct shim_effect_if {
+    int (*process)(effect_handle_t, audio_buffer_t *, audio_buffer_t *);
+    int (*command)(effect_handle_t, uint32_t, uint32_t, void *, uint32_t *, void *);
+};
+
+static struct shim_effect_if gEffectIf = { effect_process, effect_command };
+static struct shim_effect_if *gEffectIfPtr = &gEffectIf;
 
 static bool uuid_equal(const effect_uuid_t *a, const effect_uuid_t *b) {
     return memcmp(a, b, sizeof(effect_uuid_t)) == 0;
 }
 
-static int ensure_lib_loaded() {
-    if (gSwDapLib != NULL) return 0;
-
-    gLibSwDap = dlopen(LIBSWDAP_PATH, RTLD_NOW);
-    if (gLibSwDap == NULL) {
-        ALOGE("dolby_shim: failed to dlopen %s: %s", LIBSWDAP_PATH, dlerror());
-        return -ENOENT;
-    }
-
-    gSwDapLib = (audio_effect_library_t *)dlsym(gLibSwDap, LIBSWDAP_SYM);
-    if (gSwDapLib == NULL) {
-        ALOGE("dolby_shim: no %s in %s", LIBSWDAP_SYM, LIBSWDAP_PATH);
-        dlclose(gLibSwDap);
-        gLibSwDap = NULL;
-        return -ENOENT;
-    }
-
-    ALOGI("dolby_shim: loaded %s", LIBSWDAP_PATH);
+static int shim_create_effect(const effect_uuid_t *uuid,
+        int32_t sessionId, int32_t ioId, effect_handle_t *pHandle) {
+    (void)sessionId; (void)ioId;
+    if (!uuid_equal(uuid, &kUuidDap)) return -ENOENT;
+    *pHandle = &gEffectIfPtr;
+    ALOGI("dolby_shim: create_effect OK for DAP");
     return 0;
 }
 
-static int shim_create_effect(const effect_uuid_t *uuid, int32_t sessionId,
-        int32_t ioId, effect_handle_t *pHandle) {
-    if (uuid_equal(uuid, &kUuidDap)) {
-        int ret = ensure_lib_loaded();
-        if (ret != 0) return ret;
-        ALOGV("dolby_shim: create_effect delegating to swdap");
-        return gSwDapLib->create_effect(uuid, sessionId, ioId, pHandle);
-    }
-    ALOGW("dolby_shim: create_effect unsupported UUID");
-    return -ENOENT;
-}
-
 static int shim_release_effect(effect_handle_t handle) {
-    return gSwDapLib ? gSwDapLib->release_effect(handle) : -ENOENT;
+    (void)handle;
+    return 0;
 }
 
 static int shim_get_descriptor(const effect_uuid_t *uuid,
-        effect_descriptor_t *pDescriptor) {
-    if (uuid_equal(uuid, &kUuidDap)) {
-        int ret = ensure_lib_loaded();
-        if (ret != 0) return ret;
+        effect_descriptor_t *pDesc) {
+    if (!uuid_equal(uuid, &kUuidDap)) return -ENOENT;
 
-        ret = gSwDapLib->get_descriptor(uuid, pDescriptor);
-        if (ret != 0) {
-            ALOGE("dolby_shim: get_descriptor failed: %d", ret);
-            return ret;
-        }
+    memset(pDesc, 0, sizeof(effect_descriptor_t));
+    pDesc->type = kUuidPatchedType;
+    pDesc->uuid = kUuidDap;
+    pDesc->apiVersion = 0x00020000;
+    pDesc->flags = 0x00040288;
+    pDesc->cpuLoad = 0;
+    pDesc->memoryUsage = 0;
+    memcpy(pDesc->name, "DAP", 3);
+    memcpy(pDesc->implementor, "Dolby Laboratories", 18);
 
-        pDescriptor->type = kUuidType;
-        pDescriptor->uuid = kUuidDap;
-
-        ALOGI("dolby_shim: get_descriptor patched type+uuid for DAP");
-        return 0;
-    }
-    ALOGW("dolby_shim: get_descriptor unsupported UUID");
-    return -ENOENT;
+    ALOGI("dolby_shim: get_descriptor OK for DAP (patched type)");
+    return 0;
 }
 
-audio_effect_library_t AELI = {
-    .create_effect = shim_create_effect,
-    .release_effect = shim_release_effect,
-    .get_descriptor = shim_get_descriptor,
+struct aeli_s {
+    uint32_t magic;
+    uint32_t version;
+    const char *lib_name;
+    const char *vendor;
+    uint8_t reserved[32];
 };
+
+static const char kAeliLibName[] = "Effect DAP Library";
+static const char kAeliVendor[] = "Dolby Laboratories";
+
+extern "C" {
+    __attribute__((visibility("default")))
+    audio_effect_library_t AUDIO_EFFECT_LIBRARY_INFO = {
+        shim_create_effect,
+        shim_release_effect,
+        shim_get_descriptor,
+    };
+
+    __attribute__((visibility("default")))
+    struct aeli_s AELI = {
+        0x41454c54,
+        0x00030000,
+        kAeliLibName,
+        kAeliVendor,
+        {0},
+    };
+}
